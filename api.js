1a9c5d3d76fe64e6f6aa378d1cd84f2ac80ddd6f
Gabriel Milhomem <gabriell.mil@gmail.com>
2020-11-11 21:48:20 -0300

    Chore: POST /participants

---
+++ api.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const stripHtml = require('string-strip-html');
+const dayjs = require('dayjs');
+
+server.use(cors());
+server.use(express.json());
+server.listen(3000);
+
+const server = express();
+let user;
+let messages = [];
+
+const toStrip = (string) => {
+    return stripHtml(string).result.trim();
+}
+
+server.post('/participants', (req, res) => {
+    let { name } = req.body;
+    name = toStrip(name);
+    if (name.length === 0) {
+        return res.sendStatus(400);
+    }
+    const time = dayjs().format('HH:mm:ss');
+    user = {name, lastStatus: Date.now()};
+    messages.push({
+        from: name, 
+        to: 'Todos',
+        text: 'entra na sala...', 
+        type: 'status',
+        time
+    });
+    return res.sendStatus(200);
+});
